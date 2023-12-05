7958304ec4ce28c91202026051fff4f9b222827f
RudinaWaslallah <rudinawasx@gmail.com>
2023-12-05 22:10:20 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-   
+   <h1>hi</h1>
   );
 }
 
-export default App;
+export default App
